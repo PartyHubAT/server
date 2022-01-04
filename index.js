4be3116c020bf4d7c087c4b74af6e29abc73c92a
ComradeVanti <kingvanti650@gmail.com>
2022-01-04 09:07:27 +0100

    Add basic express server
    Nothing fancy here. Get the port from the env file and start the express server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+
+// Load config values
+
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+// Setup globals
+
+const app = express();
+
+// Start server
+
+(function startUp() {
+  console.log('Server starting...');
+
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
+}());
