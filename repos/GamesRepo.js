22c0e2f7dd276edb7bd2c4152cb6c869ca99b1d2
ComradeVanti <kingvanti650@gmail.com>
2022-01-05 10:51:46 +0100

    Document games-repo

--- repos/GamesRepo.js
+++ repos/GamesRepo.js
@@ -1,10 +1,25 @@
-﻿module.exports = (mongoose) => {
+﻿/**
+ * Repository for interacting with games in the database
+ * @param mongoose The mongoose instance on this server
+ * @returns {{getAll(): Promise<Object[]>, putNew(Object): Promise<Object>}}
+ */
+module.exports = (mongoose) => {
   const Game = require('../models/Game.js')(mongoose)
 
   return {
+    /**
+     * Puts a new game into the database
+     * @param {Object} game The game to put
+     * @returns {Promise<Object>} The created game
+     */
     async putNew (game) {
       return Game.create(game)
     },
+
+    /**
+     * Gets all games in the database
+     * @returns {Promise<Object[]>}
+     */
     async getAll () {
       return Game.find()
     }
