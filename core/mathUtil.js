5ae08a91df51af687c5bd20a50a1287b464369a6
ComradeVanti <kingvanti650@gmail.com>
2022-01-05 10:57:34 +0100

    Document math-util

--- core/mathUtil.js
+++ core/mathUtil.js
@@ -1,4 +1,14 @@
-﻿module.exports = {
+﻿/**
+ * Various math utility functions
+ * @type {{randBetween(number, number): number}}
+ */
+module.exports = {
+  /**
+   * Generates a random number [min;max[
+   * @param {number} min The inclusive minimum
+   * @param {number} max The exclusive maximum
+   * @returns {number} The generated number
+   */
   randBetween (min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
