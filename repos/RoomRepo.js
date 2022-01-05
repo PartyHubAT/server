25dc200e7708f9743cb8251f847395fbf16aa6ff
ComradeVanti <kingvanti650@gmail.com>
2022-01-05 10:44:54 +0100

    Document room-repo

--- repos/RoomRepo.js
+++ repos/RoomRepo.js
@@ -1,13 +1,36 @@
-﻿module.exports = (mongoose) => {
+﻿/**
+ * Repository for interacting with rooms in the database
+ * @param mongoose The mongoose instance on this server
+ * @returns {{putNew(Object): Promise<Object>, getById(*): Promise<Object|undefined>, updateById(number, Object): Promise<void>}}
+ */
+module.exports = (mongoose) => {
   const Room = require('../models/Room.js')(mongoose)
 
   return {
+    /**
+     * Puts a new room into the database
+     * @param {Object} room The room to put
+     * @returns {Promise<Object>} The created room
+     */
     async putNew (room) {
       return Room.create(room)
     },
+
+    /**
+     * Gets a specific room by id
+     * @param id The id of the room
+     * @returns {Promise<Object|undefined>} The room or undefined if not found
+     */
     async getById (id) {
       return Room.findOne({ _id: id })
     },
+
+    /**
+     * Updates a room in the database
+     * @param {number} id The id of the room
+     * @param {Object} update The updated room data
+     * @returns {Promise<void>}
+     */
     async updateById (id, update) {
       await Room.findByIdAndUpdate(id, update)
     }
