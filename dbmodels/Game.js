fb2c819f5040e5ac501b4e503848c74df6e79a20
ComradeVanti <kingvanti650@gmail.com>
2022-01-10 09:55:43 +0100

    Drop unused code

--- dbmodels/Game.js
+++
@@ -1,9 +0,0 @@
-﻿/**
- * Initializes the model for games
- * @param mongoose The mongoose instance on this server
- * @returns {Object} The model
- */
-module.exports = (mongoose) => mongoose.model('Game', new mongoose.Schema({
-  name: { type: String, required: true }
-},
-{ collection: 'games' }))
