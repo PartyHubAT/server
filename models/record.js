2226713ca83581b8372c9122a035a1c4e7b54b12
ComradeVanti <kingvanti650@gmail.com>
2022-01-10 08:40:27 +0100

    Add basic record logic

---
+++ models/record.js
@@ -0,0 +1,44 @@
+﻿/**
+ * Creates a new record
+ * @param {Object} obj The object from which to create the record
+ * @constructor
+ */
+function Record (obj) {
+  Object.keys(obj).forEach(key => { this[key] = obj[key] })
+}
+
+/**
+ * Makes a new record
+ * @param {Object} obj The object from which to create the record
+ * @return {Record} The created record
+ */
+function make (obj) {
+  const record = new Record(obj)
+  Object.freeze(record)
+  return record
+}
+
+/**
+ * Mutates a field in this record
+ * @param {string} field The name of the field
+ * @param {function} mutator The mutator function for the field
+ * @return {Record} A new record, with the mutated field
+ */
+Record.prototype.mutateField = function (field, mutator) {
+  const clone = { ...this }
+  clone[field] = mutator(clone[field])
+  return make(clone)
+}
+
+/**
+ * Mutates a record
+ * @param {function} mutate The mutation function
+ * @return {Record} A modified copy of the record
+ */
+Record.prototype.mutate = function (mutate) {
+  const clone = { ...this }
+  mutate(clone)
+  return make(clone)
+}
+
+module.exports = { make }
