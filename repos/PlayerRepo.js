27eed9de6164299c2895fe17abae2eef91f8b222
ComradeVanti <kingvanti650@gmail.com>
2022-01-20 11:47:17 +0100

    Add better error-handling to player-repo

--- repos/PlayerRepo.js
+++ repos/PlayerRepo.js
@@ -1,4 +1,6 @@
-﻿/**
+﻿const PlayerNotFoundError = require('./errors/PlayerNotFoundError')
+
+/**
  * Stores and retrieves players
  */
 class PlayerRepo {
@@ -18,8 +20,8 @@ class PlayerRepo {
 
   /**
    * Puts a new player into the database
+   * @async
    * @param {Player} player The player to put
-   * @returns {Promise}
    */
   async putNew (player) {
     await this.#playerModel.create(player)
@@ -27,30 +29,37 @@ class PlayerRepo {
 
   /**
    * Gets a player by their id
+   * @async
    * @param {PlayerId} id The id of the player
-   * @returns {Promise<Player>} The player
+   * @returns {Player} The player
+   * @throws {PlayerNotFoundError} The player with the given id was not found
    */
   async tryGetById (id) {
     return this.#playerModel.findById(id).exec()
+      .catch(_ => { throw new PlayerNotFoundError(id) })
   }
 
   /**
    * Deletes a specific player
+   * @async
    * @param {PlayerId} id The id of the player to delete
-   * @returns {Promise}
+   * @throws {PlayerNotFoundError} The player with the given id was not found
    */
   async tryDeleteById (id) {
     await this.#playerModel.findByIdAndDelete(id).exec()
+      .catch(_ => { throw new PlayerNotFoundError(id) })
   }
 
   /**
    * Updates a specific player in the database
+   * @async
    * @param {PlayerId} id The id of the player
    * @param {Object} update The new player data
-   * @returns {Promise}
+   * @throws {PlayerNotFoundError} The player with the given id was not found
    */
   async tryUpdateById (id, update) {
     await this.#playerModel.findByIdAndUpdate(id, update).exec()
+      .catch(_ => { throw new PlayerNotFoundError(id) })
   }
 }
 
---
+++ repos/errors/PlayerNotFoundError.js
@@ -0,0 +1,21 @@
+﻿/**
+ * Error for when a player was not found in the database
+ */
+class PlayerNotFoundError extends Error {
+  /**
+   * The id of the missing player
+   * @type {PlayerId}
+   */
+  playerId
+
+  /**
+   * Initialized a new error
+   * @param {PlayerId} playerId The id of the missing player
+   */
+  constructor (playerId) {
+    super(`No player with the id "${playerId}" was found.`)
+    this.playerId = playerId
+  }
+}
+
+module.exports = PlayerNotFoundError
