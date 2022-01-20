2834682cc41bdb47e43a3033622705ba3e6d0b92
ComradeVanti <kingvanti650@gmail.com>
2022-01-20 11:52:08 +0100

    Add better error-handling to room-repo

---
+++ errors/RoomNotFoundError.js
@@ -0,0 +1,21 @@
+﻿/**
+ * Error for when a room was not found in the database
+ */
+class RoomNotFoundError extends Error {
+  /**
+   * The id of the missing room
+   * @type {RoomId}
+   */
+  roomId
+
+  /**
+   * Initialized a new error
+   * @param {RoomId} roomId The id of the missing room
+   */
+  constructor (roomId) {
+    super(`No room with the id ${roomId} was found.`)
+    this.roomId = roomId
+  }
+}
+
+module.exports = RoomNotFoundError
--- repos/RoomRepo.js
+++ repos/RoomRepo.js
@@ -1,4 +1,6 @@
-﻿/**
+﻿const RoomNotFoundError = require('../errors/RoomNotFoundError')
+
+/**
  * Stores and retrieves rooms
  */
 class RoomRepo {
@@ -18,8 +20,8 @@ class RoomRepo {
 
   /**
    * Puts a new room into the database
+   * @async
    * @param {Room} room The room to put
-   * @returns {Promise}
    */
   async putNew (room) {
     await this.#roomModel.create(room)
@@ -27,27 +29,34 @@ class RoomRepo {
 
   /**
    * Gets a specific room by id
+   * @async
    * @param {RoomId} id The id of the room
-   * @returns {Promise<Room>} The room
+   * @returns {Room} The room
+   * @throws {RoomNotFoundError} When not room with the given id was found
    */
   async tryGetById (id) {
     return this.#roomModel.findById(id).exec()
+      .catch(_ => { throw new RoomNotFoundError(id) })
   }
 
   /**
    * Updates a room in the database
+   * @async
    * @param {RoomId} id The id of the room
    * @param {Object} update The updated room data
-   * @returns {Promise}
+   * @throws {RoomNotFoundError} When not room with the given id was found
    */
   async tryUpdateById (id, update) {
     await this.#roomModel.findByIdAndUpdate(id, update)
+      .catch(_ => { throw new RoomNotFoundError(id) })
   }
 
   /**
    * Gets the name of the selected game in a room
+   * @async
    * @param {RoomId} roomId The id of the room
-   * @returns {Promise<GameName>} The name of the game
+   * @returns {GameName} The name of the game
+   * @throws {RoomNotFoundError} When not room with the given id was found
    */
   async tryGetSelectedGameName (roomId) {
     return this.#roomModel
@@ -55,12 +64,15 @@ class RoomRepo {
       .select('gameName')
       .exec()
       .then(r => r.gameName)
+      .catch(_ => { throw new RoomNotFoundError(roomId) })
   }
 
   /**
    * Gets the ids of all players in a room
+   * @async
    * @param {RoomId} roomId The id of the room
-   * @returns {Promise<PlayerId[]>} The ids
+   * @returns {PlayerId[]} The ids
+   * @throws {RoomNotFoundError} When not room with the given id was found
    */
   async tryGetPlayerIdsInRoom (roomId) {
     return this.#roomModel
@@ -68,6 +80,7 @@ class RoomRepo {
       .select('playerIds')
       .exec()
       .then(r => r.playerIds)
+      .catch(_ => { throw new RoomNotFoundError(roomId) })
   }
 }
 
